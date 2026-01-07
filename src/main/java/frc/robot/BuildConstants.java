548bce51f1797317b26b5f96fca5ee50b2933f5d
Aneesh Nirmal <aneesh.nirmal10@gmail.com>
2026-01-06 21:24:46 -0500

    Reformated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Bunnybots-2025-2026-Kitbot-WPILIB2026Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-05 18:04:09 EST";
-  public static final long BUILD_UNIX_TIME = 1767654249749L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "88e5c146cd7b91a019fabf379ac746b461e447ed";
+  public static final String GIT_DATE = "2026-01-06 18:05:55 EST";
+  public static final String GIT_BRANCH = "feature/add-workflows";
+  public static final String BUILD_DATE = "2026-01-06 21:23:41 EST";
+  public static final long BUILD_UNIX_TIME = 1767752621173L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
